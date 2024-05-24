fdde5e5984edc59d64c8be36c1b77289eb19b41a
Kamyar <139503563+GlobularOne@users.noreply.github.com>
2024-05-24 17:55:30 +0330

    Update bios_dis_encryption_message.mk: Add clean routine

--- src/bios_disk_encryption_message.mk
+++ src/bios_disk_encryption_message.mk
@@ -6,3 +6,6 @@ build: bios_disk_encryption_message.bin
 
 bios_disk_encryption_message.bin:
 	$(NASM) -f bin $(NASM_FLAGS) bios_disk_encryption_message.nasm -o bios_disk_encryption_message.bin
+
+clean:
+	rm -f bios_disk_encryption_message.bin
